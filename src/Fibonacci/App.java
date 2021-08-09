4fb1504b418d707e5334715d04b2a6ac5981ea46
salihb152 <salih.bicer@ibnhaldun.edu.tr>
2021-08-09 03:39:26 +0300

    fibonacci added

---
+++ src/Fibonacci/App.java
@@ -0,0 +1,21 @@
+package Fibonacci;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int num= sc.nextInt();
+        int prevTotal=0;
+        int nextTotal;
+        int total=1; 
+        int count=2;
+        while(count<num){
+            nextTotal=prevTotal+total;
+            System.out.println(prevTotal+ " + "+ total+ " = "+ nextTotal);
+            prevTotal=total;
+            total=nextTotal;
+            count++;
+        }
+    }
+}
