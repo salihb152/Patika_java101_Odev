494d637254244dfb4cde402f58f3d284ee1ae09c
salihb152 <salih.bicer@ibnhaldun.edu.tr>
2021-08-14 02:48:32 +0300

    Mayın tarlası added

---
+++ src/MayinTarlasiOyunu/Main.java
@@ -0,0 +1,25 @@
+package MayinTarlasiOyunu;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.print("Oyunun satır uzunluğunu giriniz : ");
+        int row= scan.nextInt();
+        System.out.print("Oyunun sütun uzunluğunu giriniz : ");
+        int column=scan.nextInt();
+        if(column>=3&&row>=3){
+            MineSweeper m=new MineSweeper(row, column);
+            m.print();
+            m.run();
+        }else{
+            System.out.println("satır ve sütun 3 veya daha büyük olmalıdır");
+        }
+        scan.close();
+    }
+    
+}
